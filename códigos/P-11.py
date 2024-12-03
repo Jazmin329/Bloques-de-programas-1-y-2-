ab179fc082b3b39e3a749e724b3707cc9c6f021e
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 17:10:38 -0600

    Create P-11.py

---
+++ códigos/P-11.py
@@ -0,0 +1,33 @@
+# Programa 11
+# Fecha: 2024/10/18
+# Elaborado por: Jazmin Macias Sabas 
+
+print("-----Operador IGUAL (==)")
+print(5 == 5)   # True
+print(10 == 7)  # False
+print(5.0 == 5) # True
+
+print("\n-----Operador DIFERENTE o NO IGUAL (!=)")
+print(5 != 5)   # False
+print(10 != 7)  # True
+print(5.0 != 5) # False
+
+print("\n-----Operador MAYOR QUE (>)")
+print(10 > 5) # True
+print(5 > 10) # False
+
+print("\nOperador MENOR QUE (<)")
+print(5 < 10) # True
+print(10 < 5) # False
+
+print("\nOperador MENOR O IGUAL QUE (<=)")
+print(5 <= 5)   # True
+print(10 <= 7)  # False
+print(3 <= 5)   # True
+
+print("\nOperador MAYOR O IGUAL QUE (>=)")
+print(5 >= 5)   # True
+print(10 >= 7)  # False
+print(3 >= 5)   # True
+
+print("\u263A" * 10)
