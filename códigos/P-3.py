0bdd9419c9edf637dd9e1033c1d6a354cb0a9ce1
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 17:04:43 -0600

    Create P-3.py

---
+++ códigos/P-3.py
@@ -0,0 +1,6 @@
+# Programa 3 Programa que imprime el número de días de una semana el nombre de una figura de 3 lados y equivalencia de 250 cm en metros
+# Fecha: 2024/10/14
+# Elaborado por: Jazmin Macias Sabas 
+print(7)
+print("Triángulo")
+print(0.25)
