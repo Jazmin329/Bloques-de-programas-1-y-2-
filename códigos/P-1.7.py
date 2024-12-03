abd8a8a8ef8695b6d1c53f01afc496e9a3598d12
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 17:21:12 -0600

    Create P-1.7.py

---
+++ códigos/P-1.7.py
@@ -0,0 +1,7 @@
+# Programa 1.7 Programa que solicite la edad e indique si puede entrar un bar
+# Fecha: 2024/10/24
+# Elaborado por: Jazmin Macias Sabas 
+edad = int(input("Ingresa tu edad: "))
+if edad >=18:print("Puede ingresar al bar")
+else:print("Vete a tu casa")
+print("Fin del programa")
