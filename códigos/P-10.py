9ffb4912d288069bab7f55fcf9ac506663ea791f
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 17:10:03 -0600

    Create P-10.py

---
+++ códigos/P-10.py
@@ -0,0 +1,11 @@
+# Programa 10
+# Fecha: 2024/10/17
+# Elaborado por: Jazmin Macias Sabas 
+print ("Hola,\n\"Osvaldo\"")
+# alt 165 para la ñ 
+# alt 92 para la b 
+# alt 94 para ^
+# alt 
+#
+print ("Estoy feliz \u263A\t\u2615\t\u2650")
+print("Alineado:\t\u263A")
