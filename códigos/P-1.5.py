703eb9287cfac3bf6c1d466f86060129df276a8b
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 17:20:08 -0600

    Create P-1.5.py

---
+++ códigos/P-1.5.py
@@ -0,0 +1,24 @@
+# Programa 1.5  Comparación
+# Fecha:2024/10/23
+# Elaborado por: Jazmin Macias Sabas 
+
+#Comparación:
+print ("perro" == "perro")
+print ("perro" != "gato")
+print ("Aguascalientes" < "Zacatecas")
+print ("Aire" >= "Agua")
+
+#Membership 
+#operador in 
+#revisa si la primer cadena está en la segunda
+print ("house" in "boathouse")
+print ("bien" in "bienvenidos")
+print ("y" not in "ejes")
+print ("je" not in "ejes")
+
+#Slicing (Rebanar) 
+#sirve para obtener un fragmento de una cadena dada. Si solo deseamos obtener un solo carácter, entonces usaremos la indexación 
+#Indexing (indexado) 
+mi_nombre = "Choto Chorchis"
+print (mi_nombre[3])
+print (mi_nombre[12])
