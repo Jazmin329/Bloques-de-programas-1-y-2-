9c7b6bfd78fdc67427de709aa23e535f6d756ab0
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 17:20:43 -0600

    Create P-1.6.py

---
+++ códigos/P-1.6.py
@@ -0,0 +1,8 @@
+# Programa 1.6 word = hamster
+# Fecha: 2024/10/23
+# Elaborado por: Jazmin Macias Sabas 
+word = "hamster"
+print (word[-1])
+print (word[1:-1])
+print (word[-3:])
+print (word[:3])
