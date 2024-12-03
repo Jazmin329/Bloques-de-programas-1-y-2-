d54b55b5e6fcd5715f7746e6a6959e8ad9dd61bb
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 17:18:57 -0600

    Create P-1.3.py

---
+++ códigos/P-1.3.py
@@ -0,0 +1,5 @@
+# Programa 1.3 Programa que comprueba el funcionamiento de la adición o concatenación  
+# CONCATENACIÓN de varias cadenas de texto 
+# Fecha: 2024/10/22
+# Elaborado por: Jazmin Macias Sabas 
+print ("Mi " + "nombre " + "es " + "Jazmin")
