6f902cfcfcf6e39c72beec3c8a634c86f66547bc
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 17:06:48 -0600

    Create P-6.py

---
+++ códigos/P-6.py
@@ -0,0 +1,8 @@
+# Programa 1.6 Programa que calcula el área de un triángulo 
+# Dados la base # y la altura por el usuario 
+# Fecha: 2024/10/15
+# Elaborado por: Jazmin Macias Sabas 
+base = float(input("ingrese el valor de la base:"))
+altura = float(input("ingrese el valor de la altura"))
+área   = base * altura / 2
+print("el area del triangulo es:" + str(área))
