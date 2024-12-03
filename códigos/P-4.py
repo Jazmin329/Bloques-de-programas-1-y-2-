546f13c4a2dc8a4529d19b6a07c895ce366827c0
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 17:05:25 -0600

    Create P-4.py

---
+++ códigos/P-4.py
@@ -0,0 +1,9 @@
+# Programa 4 Probar el funcionamiento del siguiente código 
+# Fecha: 2024/10/14
+# Elaborado por: Jazmin Macias Sabas 
+print(1 +2)
+print(1 -2)
+print (1*2)
+print("concatenando cadenas" +"esta bien")
+
+print("no es posible" -"restar cadenas ")
