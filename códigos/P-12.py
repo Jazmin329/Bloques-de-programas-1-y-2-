763ecf45021282c98321d18184dccde5746bc332
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 17:11:10 -0600

    Create P-12.py

---
+++ códigos/P-12.py
@@ -0,0 +1,13 @@
+# Programa 12
+# Fecha: 2024/10/18
+# Elaborado por: Jazmin Macias Sabas 
+print ("compuerta OR")
+print (False or False)
+print (False or True)
+print (True or False)
+print (True or True)
+print("compuerta AND")
+print (False and False)
+print (False and True)
+print (True and False)
+print (True and True)
