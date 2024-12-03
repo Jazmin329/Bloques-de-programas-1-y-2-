953b56b86a701e128fff7c006fa6abbdb87a0417
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 17:03:49 -0600

    Create  P-2.py

---
+++ códigos/P-2.py
@@ -0,0 +1,7 @@
+# Programa 2  Programa que solicita el nombre al usuario y lo imprime como un mensaje de texto
+# Personalizado 
+# Fecha: 2024/10/14
+# Elaborado por: Jazmin Macias Sabas 
+nombre = input ("Ingresa tu nombre:") # Método que permite ingresar texto 
+# Desde el teclado 
+print("Hola, " + nombre)
