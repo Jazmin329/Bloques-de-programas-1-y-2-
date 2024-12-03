c91009192a4f7ae6ac25d078474924bc1c33090b
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 17:18:22 -0600

    Create P-1.2.py

---
+++ códigos/P-1.2.py
@@ -0,0 +1,14 @@
+# Programa 1.2 Programa que si Chorchis o Choto van a la fiesta 
+# Fecha: 2024/10/22
+# Elaborado por: Jazmin Macias Sabas 
+
+#/ A / B // NOT (A or B) /
+#/ 0 / 0 // 1
+#/ 0 / 1 // 0
+#/ 1 / 0 // 0
+#/ 1 / 1 // 0 
+
+print(not(False or False)) # Si hay fiesta 
+print(not(False or True))  # No hay fiesta 
+print(not(True or False))  # No hay fiesta 
+print(not(True or True))   # No hay fiesta 
