9c35b760a0713bda79c842299bd43e2ef8eaaa2c
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 17:06:03 -0600

    Create P-5.py

---
+++ códigos/P-5.py
@@ -0,0 +1,13 @@
+# Programa 5 Programa que sume, reste, divida y multiplique 2 números 
+# Fecha: 2024/10/14
+# Elaborado por: Jazmin Macias Sabas
+num_1 = int(input("ingresa el primero número:"))
+num_2 = int(input("ingresa el segundo número:"))
+suma = num_1 + num_2
+resta = num_1 - num_2
+division = num_1 / num_2
+multiplicación = num_1 * num_2
+print("El valor de la suma es: " + str (suma))
+print("El valor de la resta es: " + str (resta))
+print("El valor de la división es: " + str (división))
+print("El valor de la multiplicación es: " + str (multiplicación))
