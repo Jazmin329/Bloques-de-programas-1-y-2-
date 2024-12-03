f40351e4fafb41abc3fc21aab887d737b6a27541
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 17:17:53 -0600

    Create P-1.1.py

---
+++ códigos/P-1.1.py
@@ -0,0 +1,5 @@
+# Programa 1.1 Demuestre el funcionamiento compuerta NOT 
+# Fecha: 2024/10/22
+# Elaborado por: Jazmin Macias Sabas 
+print (not True)  # Imprime False 
+print (not False) # Imprime True 
